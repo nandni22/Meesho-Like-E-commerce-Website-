b5e1d6f69d1c72f47d7c40d0a3d6c6adb83eecec
nandni22 <nandni60663@gmail.com>
2025-01-24 21:10:02 +0530

    Add files via upload

---
+++ lernjs1.js
@@ -0,0 +1,70 @@
+//FOR RUNING THE CODE IN TERMINAL 
+               // TYPE:   " Node lernjs1.js"  AND PRESS Enter key
+
+
+
+                  
+// console.log("sumit");                       //to print something
+// a={name:"sumit kumar",roll:6,branch:"ECE"}  //OBJECT
+// console.log(a.roll);
+// b=["sumit",3,5,5.6,'d']                     //ARRAY
+// console.log(b[4]);
+
+//****DATA TYPE****/
+
+// var name="sumit";           //variable name should be start with alphabet,undercase or dollor
+// var ram="amit";
+// var roll=20;
+// var marks=78.9;
+// let room=105;
+// const cgpa=8.5;             //VALUE OF CONST CAN NOT BE CHANGED
+
+// console.log(name);
+// code='s';
+// console.log(typeof marks);
+// console.log(typeof(a));
+// console.log(typeof(b));             //"typeof" used for finding the type of variable
+// console.log(typeof(room));
+// console.log(typeof(cgpa));
+
+
+// a=2;   //(here all assigment is used ex:=,+=,*=,/=,%=,**=)
+// a**=5;  //  i.e.  a=a**5; or a=a^5
+// console.log(a);
+ 
+// also all arthmetic is used ex:+,-,%,**(power),++,__(decreasement),etc;
+// a=445;
+// b='78';
+// d=Number(c=a+b);
+// console.log(c);             // here c is string type 
+// console.log(typeof d);      // here d is number type
+
+// console.log(12+5);  
+// console.log(12+"23");       //string addition
+// console.log(12+'4');
+// a=5;
+// b='5'
+// console.log(a===b);         //also check datatype
+// console.log(a==b);          //only check value
+
+   // other comparison operators are ex: !=,!==,>,>=,<,<= etc;
+
+  
+//    console.log(false==0);
+//    console.log(false==null);
+//    console.log(false==undefined);
+
+ // false value are: false ,null,undefined,0,''  but not equal to false except 0
+
+// console.log(true && "sumit" && undefined && 0 && false && 6); // && finds first false otherwise give last true value
+// console.log(false || 0 || 6 || true || undefined); //finds first true 
+
+// BITWISE OPERATOR  IN JS ARE : &,|,^,~,<<,>>(AND,OR,XOR,NOT,LEFT SHIFT, RIGHT SHIFT)
+// a=3;
+// b=4;
+// console.log(a&b);
+// console.log(a|b);
+// console.log(a^b);
+// console.log(~8);               // value is -(8+1)
+// console.log(a<<1); // here a=3=in binary(011)->after left sifting by 1 bit(0110)=in decimal(6)
+
